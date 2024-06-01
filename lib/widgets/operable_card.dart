097b0c773ff6700a8da662e33d250d06b9e76b0f
nohminist <nohminism@gmail.com>
2024-06-01 16:19:24 +0900

    hover fix

--- lib/widgets/operable_card.dart
+++ lib/widgets/operable_card.dart
@@ -19,59 +19,48 @@ class OperableCard extends StatefulWidget {
 }
 
 class _OperableCardState extends State<OperableCard> {
-  bool _isTapped = false; // タップ状態を追加
+  late Offset _lastHoverPosition;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (_) {
-        // タップ開始時にタップ状態を設定
-        setState(() {
-          _isTapped = true;
-        });
-      },
-      onTapUp: (_) {
-        // タップ終了時にタップ状態を解除
-        setState(() {
-          _isTapped = false;
-        });
-        Provider.of<HoverCardManage>(context, listen: false).deselectCardNo();
-        widget.onTap();
-      },
-      onLongPress: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return CardDetailModal(
-                cards: widget.cards, initialCardNo: widget.cardNo);
-          },
-        );
-      },
-      onPanStart: (_) {
-        Provider.of<HoverCardManage>(context, listen: false).startDragging();
-      },
-      onPanEnd: (_) {
-        Provider.of<HoverCardManage>(context, listen: false).stopDragging();
-      },
-      child: MouseRegion(
-        onHover: (_) {
-          if (_isTapped) {
-            // タップ状態のときはonHoverイベントを無視
-            return;
-          }
+    return MouseRegion(
+      onHover: (event) {
+        if (_lastHoverPosition == event.position) {
+          return;
+        }
+        _lastHoverPosition = event.position;
 
-          var screenWidth = MediaQuery.of(context).size.width;
-          var hoverPosition = _.position.dx;
-          var isLeftSideSelected = hoverPosition < (screenWidth / 2);
-          Provider.of<HoverCardManage>(context, listen: false)
-              .selectCardNo(widget.cardNo, isLeftSideSelected);
+        var screenWidth = MediaQuery.of(context).size.width;
+        var hoverPosition = event.position.dx;
+        var isLeftSideSelected = hoverPosition < (screenWidth / 2);
+        Provider.of<HoverCardManage>(context, listen: false)
+            .selectCardNo(widget.cardNo, isLeftSideSelected);
+      },
+      onExit: (_) {
+        Provider.of<HoverCardManage>(context, listen: false).deselectCardNo();
+      },
+      child: GestureDetector(
+        onPanStart: (_) {
+          Provider.of<HoverCardManage>(context, listen: false).startDragging();
         },
-        onExit: (_) {
+        onPanEnd: (_) {
+          Provider.of<HoverCardManage>(context, listen: false).stopDragging();
+        },
+        onTap: () {
           Provider.of<HoverCardManage>(context, listen: false).deselectCardNo();
+          widget.onTap();
+        },
+        onLongPress: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return CardDetailModal(
+                  cards: widget.cards, initialCardNo: widget.cardNo);
+            },
+          );
         },
         child: CardImage9(cardNo: widget.cardNo),
       ),
     );
   }
 }
-
